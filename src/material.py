240656ec21f740eed2039628343cd5f8b520f64a
Felix iltgen <felix.horst.iltgen@hs-furtwangen.de>
2022-07-12 14:56:06 +0200

    Kommentare

--- src/material.py
+++ src/material.py
@@ -12,25 +12,30 @@ def createWindowMaterialNight():
     material = bpy.data.materials.new(name='Licht_Fenster')
     material.use_nodes=True
 
+    #Delete Principled BSDF
     mat = bpy.data.materials['Licht_Fenster']
     node_to_delete =  mat.node_tree.nodes['Principled BSDF']
     mat.node_tree.nodes.remove( node_to_delete )
 
+    #Add Emission Shader
     emission_Node = mat.node_tree.nodes.new('ShaderNodeEmission')
     emission_Node.location = (0,320)
 
+    #Add right values to the shader
     emission_Node.inputs[0].default_value = (1.0,0.534,0.029,1.0)
     emission_Node.inputs[1].default_value = 4.0
     
+    #Conect nodes
     shaderOutput = mat.node_tree.nodes.get('Material Output')
     mat.node_tree.links.new(emission_Node.outputs[0], shaderOutput.inputs[0] )
     
+    #Configure scene settings 
     eeveeObj = bpy.data.scenes['Scene'].eevee
     eeveeObj.use_bloom = True
     eeveeObj.bloom_color = (1.0,0.425,0.006)
     eeveeObj.bloom_intensity = 0.5
 
-createWindowMaterialNight()    
+  
     
     
     
